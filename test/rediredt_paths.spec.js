aabb9418d8faf661ceb8524ce9df8b96845034fb
Lucas Chen <lucas.chen.formal@gmail.com>
2020-09-08 09:34:14 +0800

    establish redirect to asset function

--- test/rediredt_paths.spec.js
+++ test/rediredt_paths.spec.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var expect = require('chai').expect;
-var SecurityChecker = require('..');
+const expect = require('chai').expect;
+const SecurityChecker = require('..');
 
 describe('RedirectPathMiddleware', () => {
-  var redirectPathMiddleware = Object;
-  var error = Error;
-  var options = Object;
+  let redirectPathMiddleware = SecurityChecker.RedirectPathMiddleware;
+  let error = Error;
+  let options = Object;
 
   describe('new()', () => {
     beforeEach(() => {
@@ -17,6 +17,13 @@ describe('RedirectPathMiddleware', () => {
       } catch (e) {
         error = e;
       }
+    });
+
+    afterEach(() => {
+      // reset params
+      redirectPathMiddleware = Object;
+      error = Error;
+      options = Object;
     });
 
     describe('when input valid params', () => {
@@ -38,21 +45,96 @@ describe('RedirectPathMiddleware', () => {
         expect(error.name).equal('ValidationError');
       });
     });
+
+    describe('when given paths is not array', () => {
+      before(() => {
+        options = {
+          paths: 'example.com',
+        };
+      });
+      it('should occur error exception', () => {
+        expect(error.name).equal('ValidationError');
+      });
+    });
   });
 
   describe('paths()', () => {
-    before(() => {
-      options = {
-        paths: ['example/path'],
-      };
+    beforeEach(() => {
+      try {
+        redirectPathMiddleware = new SecurityChecker.RedirectPathMiddleware({
+          paths: ['example/path'],
+        });
+      } catch (e) {
+        error = e;
+      }
     });
+
+    afterEach(() => {
+      // reset params
+      redirectPathMiddleware = Object;
+      error = Error;
+      options = Object;
+    });
+
     it('should returns paths', () => {
+      expect(error.message).to.be.undefined;
       expect(redirectPathMiddleware.paths.length).equal(1);
       expect(redirectPathMiddleware.paths[0]).to.have.equal('example/path');
     });
   });
 
-  describe('redirectPath()', () => {
-    it('should replace the path result', () => {});
+  describe('middleware()', () => {
+    let req = Object,
+      resp = Object;
+
+    beforeEach(() => {
+      try {
+        redirectPathMiddleware = new SecurityChecker.RedirectPathMiddleware({
+          paths: ['/shopback/resource'],
+        });
+        redirectPathMiddleware.redirectToAssetsMiddleware(req, resp, () => {});
+      } catch (e) {
+        error = e;
+      }
+    });
+
+    afterEach(() => {
+      // reset params
+      req = Object;
+      resp = Object;
+      redirectPathMiddleware = Object;
+      error = Error;
+      options = Object;
+    });
+
+    describe('when request path with params matched the config', () => {
+      before(() => {
+        req = { path: '/shopback/resource?sort=desc' };
+      });
+      it('should replace the path', () => {
+        expect(error.message).to.be.undefined;
+        expect(req.path).equal('/shopback/static/assets?sort=desc');
+      });
+    });
+
+    describe('when request close path with params matched the config', () => {
+      before(() => {
+        req = { path: '/shopback/resource/?sort=desc' };
+      });
+      it('should replace the path', () => {
+        expect(error.message).to.be.undefined;
+        expect(req.path).equal('/shopback/static/assets/?sort=desc');
+      });
+    });
+
+    describe('when request path with params partially matched the config', () => {
+      before(() => {
+        req = { path: '/shopback/resource/products/?sort=desc' };
+      });
+      it('should replace the path', () => {
+        expect(error.message).to.be.undefined;
+        expect(req.path).equal('/shopback/static/assets/products/?sort=desc');
+      });
+    });
   });
 });
